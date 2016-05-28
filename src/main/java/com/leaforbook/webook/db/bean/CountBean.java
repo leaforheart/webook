9e822586761e265164577b75b643ff5bedb2118e
xiaoyilin <leaforbook@gmail.com>
2016-05-28 14:43:23 +0800

    bean

---
+++ src/main/java/com/leaforbook/webook/db/bean/CountBean.java
@@ -0,0 +1,13 @@
+package com.leaforbook.webook.db.bean;
+
+public class CountBean {
+	private int count;
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+}
